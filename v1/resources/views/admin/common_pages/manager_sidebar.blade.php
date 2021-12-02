3aaf413066540760dc3fa74761169d17c875830b
jayaraju <jayaraju@deepredink.com>
2021-12-02 10:29:17 +0530

    Update manager_sidebar.blade.php

--- v1/resources/views/admin/common_pages/manager_sidebar.blade.php
+++ v1/resources/views/admin/common_pages/manager_sidebar.blade.php
@@ -23,6 +23,11 @@
 <li class="{{ (request()->is(Config::get('constants.admin').'/departments')) ? 'active' : '' }}"><a href="{{url(Config::get('constants.admin').'/departments')}}">Lists</a></li>
 </ul>
 </li>
+<li class="{{ (request()->is(Config::get('constants.admin').'/users')) ? 'active' : '' }}">
+<a href="{{url(Config::get('constants.admin').'/users')}}">
+<img src="{{URL::to('assets/images/svg-icon/user.svg')}}" class="img-fluid" alt="apps"><span>Users</span><i class="feather"></i>
+</a>
+</li>
 <li>
 <a href="javaScript:void();">
 <img src="{{URL::to('assets/images/svg-icon/basic.svg')}}" class="img-fluid" alt="apps"><span>Surveys</span><i class="feather icon-chevron-right pull-right"></i>
@@ -34,11 +39,7 @@
 <li class="{{ (request()->is(Config::get('constants.admin').'/questions_options')) ? 'active' : '' }}"><a href="{{url(Config::get('constants.admin').'/questions_options')}}">Questions Options</a></li>
 </ul>
 </li>
-<li class="{{ (request()->is(Config::get('constants.admin').'/users')) ? 'active' : '' }}">
-<a href="{{url(Config::get('constants.admin').'/users')}}">
-<img src="{{URL::to('assets/images/svg-icon/user.svg')}}" class="img-fluid" alt="apps"><span>Users</span><i class="feather"></i>
-</a>
-</li>
+
 <li>
 <a href="{{route('session.logout')}}">
 <img src="{{URL::to('assets/images/svg-icon/logout.svg')}}" class="img-fluid" alt="apps"><span>Logout</span><i class="feather "></i>
