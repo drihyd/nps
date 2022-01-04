0f6851b06485bc9eedcc0d3d7804101edde7d74e
jayaraju <jayaraju@deepredink.com>
2022-01-04 15:25:40 +0530

    Update responses_status_activities_common.blade.php

--- v1/resources/views/admin/responses/responses_status_activities_common.blade.php
+++ v1/resources/views/admin/responses/responses_status_activities_common.blade.php
@@ -16,14 +16,24 @@
                 <div class="activities-history-item">
 
                     @if($person_responses_status->ticket_status=="opened")
-                    <h6 class="badge badge-danger">{{$person_responses_status->ticket_status}}</h6>
-                    @elseif($person_responses_status->ticket_status=="closed_satisfied" || $person_responses_status->ticket_status=="closed_unsatisfied")
-                    <h6 class="badge badge-success">{{$person_responses_status->ticket_status}}</h6>
-                    <p><b>Remarks:</b>{{$person_responses_status->ticket_remarks??''}}</p>
-                    @else
-                    <h6 class="badge badge-primary">{{$person_responses_status->ticket_status}}</h6>
+                    <h6 class="badge badge-danger">Opened</h6>
+                    @elseif($person_responses_status->ticket_status== "phone_ringing_no_response")
+                    <h6 class="badge badge-primary">Phone Ringing - No Response</h6>
                     <p><b>Remarks: </b>{{$person_responses_status->ticket_remarks??''}}</p>
-                    @endif                                            
+                    @elseif($person_responses_status->ticket_status=="connected_refused_to_talk")
+                    <h6 class="badge badge-primary">Connected - Refused to talk</h6>
+                    <p><b>Remarks: </b>{{$person_responses_status->ticket_remarks??''}}</p>
+                    @elseif($person_responses_status->ticket_status=="connected_asked_for_call_back")
+                    <h6 class="badge badge-primary">Connected - Asked for call back</h6>
+                    <p><b>Remarks: </b>{{$person_responses_status->ticket_remarks??''}}</p>
+                    @elseif($person_responses_status->ticket_status=="closed_satisfied")
+                    <h6 class="badge badge-success">Closed - Satisfied</h6>
+                    <p><b>Remarks: </b>{{$person_responses_status->ticket_remarks??''}}</p>
+                    @elseif($person_responses_status->ticket_status=="closed_unsatisfied")
+                    <h6 class="badge badge-success">Closed - Unsatisfied</h6>
+                    <p><b>Remarks: </b>{{$person_responses_status->ticket_remarks??''}}</p>
+                    @else
+                    @endif                                          
                     
                     <p class="mb-0">{{ date('G:i A.', strtotime($person_responses_status->created_at??'')) }} - {{date('F j, Y', strtotime($person_responses_status->created_at??''))}}</p>
                 </div>
