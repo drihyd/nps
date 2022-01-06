04ba531118c924e355b178122400a2a479610fa5
jayaraju <jayaraju@deepredink.com>
2022-01-06 15:16:43 +0530

    changes done

--- v1/resources/views/admin/common_pages/levels.blade.php
+++ v1/resources/views/admin/common_pages/levels.blade.php
@@ -7,7 +7,7 @@ $group_level_data=GroupLevel::where('organization_id',Auth::user()->organization
 <select class="form-control" name="level" id="role">
   <option value="">-- All --</option>
   @foreach($group_level_data as $group_level)
-    <option value="{{$group_level->id??''}}" {{ $level == $group_level->id ? 'selected':''}}>{{ucwords($group_level->name??'')}}</option>
+    <option value="{{$group_level->id??''}}" {{ $level == $group_level->id ? 'selected':''}}>{{ucwords($group_level->name??'')}} - {{ucwords($group_level->alias??'')}}</option>
   @endforeach
 </select>
 </div>
