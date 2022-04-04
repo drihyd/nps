9c80664ad49d44a3e0d2f2d46141177c42749f8e
jayaraju <jayaraju@deepredink.com>
2022-04-04 09:36:50 +0530

    response view

--- v1/resources/views/admin/responses/table_lists.blade.php
+++ v1/resources/views/admin/responses/table_lists.blade.php
@@ -1,96 +1,111 @@
-
-		
-		<div class="table-responsive">
-		<table  id="default-datatable" class="nowrap display table Config::get('constants.tablestriped')">
-		<thead class="thead-dark">
-		<tr>
-		<th>S.No</th>
-		<th>Customer</th>      
-		<!-- <th>Gender</th> -->
-		<th>Ticket Number</th>
-		<th>Score</th>
-		<th>Feedback Date</th>
-
-		<th>Status</th>
-		<th>Actions</th>
-		</tr>
-		</thead>
-		<tbody>
-
-			
-                  
-                      @foreach ($Data as $response)  
-                          <tr>
-                              <td>{{$loop->iteration}}</td>
-
-                              <td><b>{{Str::title($response->firstname??'')}}</b><br>{{$response->email??''}}<br>{{$response->mobile??''}}<br>Gender: {{Str::title($response->gender??'')}}<br>UHID: {{Str::title($response->uhid??'')}}</td>
-                              <td>
-							  @if(Auth::user()->role==2)         
-							  <a href="{{url(Config::get('constants.admin').'/responses/view/'.Crypt::encryptString($response->id))}}" class="text-primary mr-2" title="Edit" >{{Str::title($response->ticker_final_number??'')}}</a><br>{{$response->survey_title??''}}<br>BED NO.: {{Str::title($response->bed_name??'')}}<br>Discharge Date: {{Str::title($response->discharge_date??'')}}
-                               
-                                @elseif(Auth::user()->role==3)
-								 <a href="{{url(Config::get('constants.hod').'/responses/view/'.Crypt::encryptString($response->id))}}" class="text-primary mr-2" title="Edit" >{{Str::title($response->ticker_final_number??'')}}</a><br>{{$response->survey_title??''}}<br>BED NO.: {{Str::title($response->bed_name??'')}}<br>Discharge Date: {{Str::title($response->discharge_date??'')}}
-                                
-                                @elseif(Auth::user()->role==4)
-                                <a href="{{url(Config::get('constants.user').'/responses/view/'.Crypt::encryptString($response->id))}}" class="text-primary mr-2" title="Edit" >{{Str::title($response->ticker_final_number??'')}}</a><br>{{$response->survey_title??''}}<br>BED NO.: {{Str::title($response->bed_name??'')}}<br>Discharge Date: {{Str::title($response->discharge_date??'')}}
-                               
-                                @else
-
-                                @endif
-							  
-							  
-							  
-							  </td>
-                              <td>{{$response->rating??0}}</td>
-                              <td>
-							  
-							  {{date('d M, Y', strtotime($response->created_at??''))}}
-							  
-							  
-							  @if($response->last_action_date)
-								  <br>
-							<p>Last Actity Date: {{date('d M, Y', strtotime($response->last_action_date??''))}}</p>
-							
-							@endif
-							
-							
-							</td>                     
-							  
-							  <td>
-							  <p class="font-15 mb-0">
-							  
-									 @include('admin.common_pages.status_of_ticket',['ticket_status'=>$response->ticket_status])
-							  
-							  </p>
-							  
-							  
-							  </td>
-							  
-                              <td>    
-                              @if(Auth::user()->role==2)         
-							  <a href="{{url(Config::get('constants.admin').'/responses/view/'.Crypt::encryptString($response->id))}}" class="text-primary mr-2" title="Edit" ><i class="feather icon-eye"></i></a>
-                                <a href="{{url(Config::get('constants.admin').'/responses/delete/'.Crypt::encryptString($response->id))}}" class="delete text-danger" title="Delete" onclick="return confirm('Are you sure to delete this?')" ><i class="feather icon-trash"></i></a>
-                                @elseif(Auth::user()->role==3)
-								<a href="{{url(Config::get('constants.hod').'/responses/view/'.Crypt::encryptString($response->id))}}" class="text-primary mr-2" title="Edit" ><i class="feather icon-eye"></i></a>
-                                <a href="{{url(Config::get('constants.hod').'/responses/delete/'.Crypt::encryptString($response->id))}}" class="delete text-danger" title="Delete" onclick="return confirm('Are you sure to delete this?')" ><i class="feather icon-trash"></i></a>
-                                @elseif(Auth::user()->role==4)
-                                <a href="{{url(Config::get('constants.user').'/responses/view/'.Crypt::encryptString($response->id))}}" class="text-primary mr-2" title="Edit" ><i class="feather icon-eye"></i></a>
-                                <a href="{{url(Config::get('constants.user').'/responses/delete/'.Crypt::encryptString($response->id))}}" class="delete text-danger" title="Delete" onclick="return confirm('Are you sure to delete this?')" ><i class="feather icon-trash"></i></a>
-                                @else
-
-                                @endif
-                              </td>
-                          </tr>
-                          @endforeach
-                      </tbody>
-                      
-            </table>
-			
-			
-			
-			
-			
-			
+<div class="row">
+<div class="col-md-8">
+	
+	<div class="card">
+		<div class="card-body p-0 text-center">
+			<div class="row no-gutters align-items-top">
+				<div class="col-auto bg-omni-black">						
+					<span class="cus-head" style="width:100px">Score</span>
+				</div>
+				<div class="col bg-omni-blue">
+					<span class="cus-head">Customer Info</span>
+				</div>
+				<div class="col bg-omni-green">
+					<span class="cus-head">Status</span>
+				</div>
 			</div>
+		</div>
+	</div>
+ @foreach ($Data as $response)  
+	<div class="card info-card bg-white border">
+		<div class="card-body p-0">
+			<div class="row no-gutters align-items-top">
+				<div class="col-sm-auto col-12">
+					<div class="cus-score">
+						<h1>5</h1>
+						@if(Auth::user()->role==2)         
+							  <a href="{{url(Config::get('constants.admin').'/responses/view/'.Crypt::encryptString($response->id))}}" class="btn btn-primary btn-sm" title="Edit" >{{Str::title($response->ticker_final_number??'')}}</a>
+					  @elseif(Auth::user()->role==3)
+						 <a href="{{url(Config::get('constants.hod').'/responses/view/'.Crypt::encryptString($response->id))}}" class="btn btn-primary btn-sm" title="Edit" >{{Str::title($response->ticker_final_number??'')}}</a>
+						 @elseif(Auth::user()->role==4)
+              <a href="{{url(Config::get('constants.user').'/responses/view/'.Crypt::encryptString($response->id))}}" class="btn btn-primary btn-sm" title="Edit" >{{Str::title($response->ticker_final_number??'')}}</a>
+              @else
+              @endif
+					</div>
+				</div>
+				<div class="col-sm col-12 bg-omni-blue-light">
+					<div class="cus-info">						
+						<span><b>{{Str::title($response->firstname??'')}}</b></span><br>
+						<span>UHID No: {{Str::title($response->uhid??'')}}</span><br>
+						<span>Bed No: {{Str::title($response->bed_name??'')}}</span><br>
+						<span><a href="mailto:{{$response->email??''}}" title="{{$response->email??''}}"><i class="fa fa-envelope"></i></a></span>
+						<span><a href="telto:{{$response->mobile??''}}" title="{{$response->mobile??''}}"><i class="fa fa-phone-square"></i></a> </span>
+					</div>
+				</div>
+				<div class="col-sm col-12 bg-omni-green-light">
+					<div class="cus-status">
+						@if(Auth::user()->role==2)         
+							  <a href="{{url(Config::get('constants.admin').'/responses/view/'.Crypt::encryptString($response->id))}}" class="text-primary mr-2" title="Edit" >@include('admin.common_pages.status_of_ticket',['ticket_status'=>$response->ticket_status])</a>
+							   @elseif(Auth::user()->role==3)
+								<a href="{{url(Config::get('constants.hod').'/responses/view/'.Crypt::encryptString($response->id))}}" class="text-primary mr-2" title="Edit" >@include('admin.common_pages.status_of_ticket',['ticket_status'=>$response->ticket_status])</a>
+								@elseif(Auth::user()->role==4)
+                                <a href="{{url(Config::get('constants.user').'/responses/view/'.Crypt::encryptString($response->id))}}" class="text-primary mr-2" title="Edit" >@include('admin.common_pages.status_of_ticket',['ticket_status'=>$response->ticket_status])</a>
+                  @else
+
+                  @endif<br>
+						<span>Updated on: {{date('d M, Y', strtotime($response->last_action_date??''))}}</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	@endforeach
+	@foreach ($Data as $response)  
+	<div class="card info-card bg-white border">
+		<div class="card-body p-0">
+			<div class="row no-gutters align-items-top">
+				<div class="col-sm-auto col-12">
+					<div class="cus-score">
+						<h1>5</h1>
+						@if(Auth::user()->role==2)         
+							  <a href="{{url(Config::get('constants.admin').'/responses/view/'.Crypt::encryptString($response->id))}}" class="btn btn-primary btn-sm" title="Edit" >{{Str::title($response->ticker_final_number??'')}}</a>
+					  @elseif(Auth::user()->role==3)
+						 <a href="{{url(Config::get('constants.hod').'/responses/view/'.Crypt::encryptString($response->id))}}" class="btn btn-primary btn-sm" title="Edit" >{{Str::title($response->ticker_final_number??'')}}</a>
+						 @elseif(Auth::user()->role==4)
+              <a href="{{url(Config::get('constants.user').'/responses/view/'.Crypt::encryptString($response->id))}}" class="btn btn-primary btn-sm" title="Edit" >{{Str::title($response->ticker_final_number??'')}}</a>
+              @else
+              @endif
+					</div>
+				</div>
+				<div class="col-sm col-12 bg-omni-blue-light">
+					<div class="cus-info">						
+						<span><b>{{Str::title($response->firstname??'')}}</b></span><br>
+						<span>UHID No: {{Str::title($response->uhid??'')}}</span><br>
+						<span>Bed No: {{Str::title($response->bed_name??'')}}</span><br>
+						<span><a href="mailto:{{$response->email??''}}" title="{{$response->email??''}}"><i class="fa fa-envelope"></i></a></span>
+						<span><a href="telto:{{$response->mobile??''}}" title="{{$response->mobile??''}}"><i class="fa fa-phone-square"></i></a> </span>
+					</div>
+				</div>
+				<div class="col-sm col-12 bg-omni-green-light">
+					<div class="cus-status">
+						@if(Auth::user()->role==2)         
+							  <a href="{{url(Config::get('constants.admin').'/responses/view/'.Crypt::encryptString($response->id))}}" class="text-primary mr-2" title="Edit" >@include('admin.common_pages.status_of_ticket',['ticket_status'=>$response->ticket_status])</a>
+							   @elseif(Auth::user()->role==3)
+								<a href="{{url(Config::get('constants.hod').'/responses/view/'.Crypt::encryptString($response->id))}}" class="text-primary mr-2" title="Edit" >@include('admin.common_pages.status_of_ticket',['ticket_status'=>$response->ticket_status])</a>
+								@elseif(Auth::user()->role==4)
+                                <a href="{{url(Config::get('constants.user').'/responses/view/'.Crypt::encryptString($response->id))}}" class="text-primary mr-2" title="Edit" >@include('admin.common_pages.status_of_ticket',['ticket_status'=>$response->ticket_status])</a>
+                  @else
+
+                  @endif<br>
+						<span>Updated on: {{date('d M, Y', strtotime($response->last_action_date??''))}}</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	@endforeach
+
+</div>
+</div>
 
 
