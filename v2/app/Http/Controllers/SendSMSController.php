d4d95c13a1ec7f20db8bff195c7e75c560f8a624
drihyd <techsupport@deepredink.com>
2022-05-05 18:23:34 +0530

    Update SendSMSController.php

--- v2/app/Http/Controllers/SendSMSController.php
+++ v2/app/Http/Controllers/SendSMSController.php
@@ -10,19 +10,8 @@ class SendSMSController extends Controller
 {
     public function index()
     {
-		
-		
+
 	
-    
-       
-		
-		
-		
-		
-		
-		
-		
-		
 //Please Enter Your Details
 $user=env('SMS_CNTRY_UN'); //your username
 $password=env('SMS_CNTRY_PWD'); //your password
@@ -34,6 +23,7 @@ $senderid="smscntry"; //Your senderid
 $messagetype="N"; //Type Of Your Message
 $DReports="Y"; //Delivery Reports
 
+
 $apiURL="http://sms.adstogether.com/app/smsapipost/index.php";
 
 $client = new \GuzzleHttp\Client();
@@ -43,10 +33,10 @@ $response = $client->request('POST', $apiURL,
 'key' => '61e6a3c7a3de0',
 'type' => 'text',
 'contacts' => '9052691535',
-'senderid' => 'OMNIHS',
-'peid' => '1201160189518122669',
-'templateid' => '123',
-'msg' => 'test message',
+'senderid' => 'KPOMNI',
+'peid' => '1201160613278000476',
+'templateid' => '1207165025919707553',
+'msg' => 'Billing new ticket #01 has a negative feedback. Cust:Venkat  Mob: 9052691535. OMNI CONNECT',
 
 
 ]
