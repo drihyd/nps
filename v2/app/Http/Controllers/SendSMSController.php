bf8676acfb311f32d120b40436247aa7c42bba9d
drihyd <techsupport@deepredink.com>
2022-05-18 18:11:20 +0530

    sms integration stated

--- v2/app/Http/Controllers/SendSMSController.php
+++ v2/app/Http/Controllers/SendSMSController.php
@@ -51,4 +51,48 @@ $responseBody = json_decode($response->getBody(), true);
 
 		
     }
+	
+	
+	
+	
+public function Send_SMS_First_Interview($params=false)
+{	
+//Please Enter Your Details
+$user=env('SMS_CNTRY_UN'); //your username
+$password=env('SMS_CNTRY_PWD'); //your password
+
+$mobilenumbers="9052691535"; //enter Mobile numbers comma seperated
+$message = "test messgae"; //enter Your Message
+$senderid="smscntry"; //Your senderid
+$messagetype="N"; //Type Of Your Message
+$DReports="Y"; //Delivery Reports
+
+
+$apiURL="http://sms.adstogether.com/app/smsapipost/index.php";
+
+$client = new \GuzzleHttp\Client();
+$response = $client->request('POST', $apiURL, 
+[
+
+'key' => '61e6a3c7a3de0',
+'type' => 'text',
+'contacts' =>$params['hod_Contact'],
+'senderid' => 'KPOMNI',
+'peid' => '1201160613278000476',
+'templateid' => '1207165025919707553',
+'msg' => $params['which_department'].' new ticket #'.$params['which_ticket'].' has a negative feedback. Cust:'.$params['patient_Name'].'  Mob: '.$params['patient_Contact'].'. OMNI CONNECT',
+]
+);
+
+
+
+$statusCode = $response->getStatusCode();
+$responseBody = json_decode($response->getBody(), true);
+
+ dd($statusCode);
+
+		
+    }
+	
+	
 }
